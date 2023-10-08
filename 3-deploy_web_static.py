1373538df769fb21a9a2151ca3d0785789fbf328
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-10-08 05:48:12 +0300

    task 2 & task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
-"""Fabric script (based on the file 1-pack_web_static.py)
-that distributes an archive to your web servers,
-using the function do_deploy"""
+""" Fabric script (based on the file 2-do_deploy_web_static.py) """
 
 from fabric.api import *
 import os.path
+from datetime import datetime
 
 
 env.user = "ubuntu"
 env.hosts = ['54.175.75.247', '18.210.14.159']
 env.key_filename = '~/.ssh/school'
+
+
+def do_pack():
+    """returns the archive path if the archive has been correctly generated.
+    Otherwise, it should return None"""
+
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    arch_name = "versions/web_static_{}.tgz".format(date)
+
+    local("mkdir -p versions")
+    result = local("tar -cvzf {} web_static".format(arch_name))
+
+    if result.failed:
+        return None
+    else:
+        return arch_name
 
 
 def do_deploy(archive_path):
@@ -28,7 +43,14 @@ def do_deploy(archive_path):
         run("sudo mv {}web_static/* {}".format(new_folder, new_folder))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -sf {} /data/web_static/current".format(new_folder))
-        puts("New version deployed!")
         return True
     except Exception:
         return False
+
+
+def deploy():
+    """creates and distributes an archive to your web servers"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
