cf0b8f8be31c36cac6f5090e484c27473ac3927f
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-10-08 03:55:40 +0300

    fixing task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,14 +19,21 @@ def do_deploy(archive_path):
 
     try:
         put(archive_path, "/tmp/")
+        print ("{} -> /tmp/".format(archive_path))
         file_name = archive_path.split("/")[-1].split(".")[0]
         new_folder = "/data/web_static/releases/" + file_name + "/"
         run("mkdir -p {}".format(new_folder))
+        print("mkdir -p {}".format(new_folder))
         run("sudo tar -xzvf /tmp/{}.tgz -C {}".format(file_name, new_folder))
+        print("sudo tar -xzvf /tmp/{}.tgz -C {}".format(file_name, new_folder))
         run("sudo rm -rf /tmp/{}.tgz".format(file_name))
+        print("sudo rm -rf /tmp/{}.tgz".format(file_name))
         run("sudo mv {}web_static/* {}".format(new_folder, new_folder))
+        print("sudo mv {}web_static/* {}".format(new_folder, new_folder))
         run("sudo rm -rf /data/web_static/current")
+        print("sudo rm -rf /data/web_static/current")
         run("sudo ln -sf {} /data/web_static/current".format(new_folder))
+        print("sudo ln -sf {} /data/web_static/current".format(new_folder))
         print("New version deployed!")
         return True
     except Exception:
