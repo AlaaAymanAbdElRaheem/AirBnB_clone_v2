8f3e9a5c32011888c4dc50239ba1ae214a45f216
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-10-22 08:01:04 +0300

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" This script starts a Flask web application."""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ This function returns a message when / is requested."""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ This function returns a message when /hbnb is requested."""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ This function returns a message when /c/<text> is requested."""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
