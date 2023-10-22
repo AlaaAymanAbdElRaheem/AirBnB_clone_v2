7db1af856210c39047edadac8d6e239815299ece
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-10-22 07:54:13 +0300

    task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,10 +13,12 @@ def hello_hbnb():
     """ This function returns a message when / is requested."""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ This function returns a message when /hbnb is requested."""
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
